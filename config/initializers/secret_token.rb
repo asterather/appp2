aff6a08090cb8e40279d790752c02fe57ff78c15
asterather <asterather@gmail.com>
2014-08-20 20:38:58 -0500

    Getting things going.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Appp2::Application.config.secret_key_base = '825e7abf64f5382c51265b234fb5cef3ac1d72e70ac04464a15e9a7b07734fb5458848a8d9d4546e3dbe999d03fdb4b7fec38b9d32bc17783f2d22f5bbf1bf8d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Appp2::Application.config.session_store :cookie_store, key: '_appp2_session'
