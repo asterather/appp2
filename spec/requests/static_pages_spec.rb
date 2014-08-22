357486569bb197d0b5d061e1a4f906ebaf1a9719
asterather <asterather@gmail.com>
2014-08-21 22:37:34 -0500

    chanced to database environments to postgres. I like postgres better than SQLite added the contact page -added a route for the contact page added onto the spec/requests/static page file for the contact page. -Contact all the things!!!

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -40,4 +40,17 @@ describe "Static pages" do
       expect(page).to have_title("Ruby on Rails Tutorial Appp2 | About Us")
     end
   end
+
+  describe "Contact page" do
+
+  	it "should have the content 'Contact Me'" do
+  		visit '/static_pages/contact' 
+  		expect(page).to have_title('Contact Me')
+  	end
+
+  	it "should have the title 'Contact Me'" do
+  		visit '/static_pages/contact'
+  		expect(page).to have_title("Ruby on Rails Tutorial Appp2 | Contact Me")
+  	end
+  end
 end
