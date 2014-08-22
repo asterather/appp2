7b67e2e0e50c07f75d9f248f5959d7feee1edc97
asterather <asterather@gmail.com>
2014-08-21 21:15:50 -0500

    created about page added route for about page -completed static pages setup made the magic happen with TDD.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,13 +8,36 @@ describe "Static pages" do
       visit '/static_pages/home'
       expect(page).to have_content('Appp2')
     end
+
+    it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Ruby on Rails Tutorial Appp2 | Home")
+    end
   end
-  
+
   describe "Help page" do
 
     it "should have the content 'Help'" do
       visit '/static_pages/help'
       expect(page).to have_content('Help')
+    end
+
+    it "should have the title 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_title("Ruby on Rails Tutorial Appp2 | Help")
+    end
+  end
+
+  describe "About page" do
+
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('About Us')
+    end
+
+    it "should have the title 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_title("Ruby on Rails Tutorial Appp2 | About Us")
     end
   end
 end
