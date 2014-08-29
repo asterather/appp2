9e47d8e7d13388fc0c6247a333952e4e552a8e92
asterather <asterather@gmail.com>
2014-08-29 14:18:32 -0500

    added def contact in static_pages_controller added routes to the URLs in the shims header and footer -modified the routes file to make the routes better and had a root changed the spec page to have root_paths instead of /go/to/file pathes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,12 +5,12 @@ describe "Static pages" do
   describe "Home page" do
 
     it "should have the content 'Appp2'" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).to have_content('Appp2')
     end
 
     it "should have the title 'Home'" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).to have_title("Ruby on Rails Tutorial Appp2 | Home")
     end
   end
@@ -18,12 +18,12 @@ describe "Static pages" do
   describe "Help page" do
 
     it "should have the content 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       expect(page).to have_content('Help')
     end
 
     it "should have the title 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       expect(page).to have_title("Ruby on Rails Tutorial Appp2 | Help")
     end
   end
@@ -31,12 +31,12 @@ describe "Static pages" do
   describe "About page" do
 
     it "should have the content 'About Us'" do
-      visit '/static_pages/about'
+      visit about_path
       expect(page).to have_content('About Us')
     end
 
     it "should have the title 'About Us'" do
-      visit '/static_pages/about'
+      visit about_path
       expect(page).to have_title("Ruby on Rails Tutorial Appp2 | About Us")
     end
   end
@@ -44,12 +44,12 @@ describe "Static pages" do
   describe "Contact page" do
 
   	it "should have the content 'Contact Me'" do
-  		visit '/static_pages/contact' 
+  		visit contact_path
   		expect(page).to have_title('Contact Me')
   	end
 
   	it "should have the title 'Contact Me'" do
-  		visit '/static_pages/contact'
+  		visit contact_path
   		expect(page).to have_title("Ruby on Rails Tutorial Appp2 | Contact Me")
   	end
   end
